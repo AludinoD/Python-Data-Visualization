2b731e128df667955cf7ffb2bb147b512a78c704
AludinoD <aludino11@gmail.com>
2026-04-28 17:25:01 +0400

    Bar Graph

--- Top Steam Games Visualization.py
+++ Top Steam Games Visualization.py
@@ -13,3 +13,50 @@ import seaborn as sns
 
 # Df is Data Frame and it uses the panda read function to read the csv file and storing it.
 df = pd.read_csv('steam_top_games_2026.csv')
+
+# Sea Born theme for the chart
+sns.set_theme(style="whitegrid")
+
+# Pre processing the data by converting the release date column to a date time format. Which helps visualize the data when the game is released, and coerece to handle any errors.
+df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+
+# Handle Missing Information by filling them with "Unknown" to avoid issues and breaking the charts.
+df['developer'] = df['developer'].fillna('Unknown') # Unknown Dev
+df['genres'] = df['genres'].fillna('Unknown') # Unknown Genre
+
+# Custom function to turn owners of the game ranges like "1,000,000 .. 2,000,000" into numbers
+# Because the dataset has the estimated_owners column in a range format instead of an integer, so we need to extract the range and convert it into a number(int)
+def extract_owners(owner_str):
+    if pd.isna(owner_str): return 0
+    try:
+        # Split by the ".." and take the first part, remove commas, and make it an integer
+        lower_bound = owner_str.split(' .. ')[0].replace(',', '')
+        return int(lower_bound)
+    except:
+        return 0
+
+# Apply the function to the estimated Owners column
+df['estimated_owners_lower'] = df['estimated_owners'].apply(extract_owners)
+
+# Create a "clean" copy of the data without rows where the date was invalid
+# Because some release dates are missing or invalid, so we create a cleaner version of the data frame that only includes rows with proper and valid release dates.
+df_clean = df.dropna(subset=['release_date']).copy()
+df_clean['release_year'] = df_clean['release_date'].dt.year
+
+
+# Graph Visualizations Section
+
+# Graph 1: Distribution of Game Prices
+# This bar chart shows how much these games costs on steam and how many games fall into each price range.
+# We filtered out the games that are under 100$ to avoid long tails on the chart, which can make it harder to see the distribution.
+plt.figure(figsize=(10, 6))
+# Filter to games under $100 to avoid long "tails" on the chart
+sns.histplot(df_clean[df_clean['price_usd'] < 100]['price_usd'], bins=30, kde=True, color='skyblue')
+# Title and Labels for the chart
+plt.title('How much do games cost? (Price Distribution)')
+plt.xlabel('Price in USD')
+plt.ylabel('Number of Games')
+plt.show() # Display the window
+
+# Based on the graph, Majority of the games are priced between 0 - 70.
+# Most famous games are priced around 20 - 60 dollars, while majority of the famous games are free which is around 50 games.
